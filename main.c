be66a670178d6636d635fafee079a3fbe08962bd
Oskar K <oskarkloczko13@gmail.com>
2024-04-08 23:15:06 +0200

    password adding and displaying functionalities

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include <pw_manager.h>
+#include "pw_manager.h"
+
+char fileName[100] = "pw_data.csv";
 
 int main() {
     bool displayMenu = true;
-
+    char websiteName[100], URL[100], userName[100], password[100], notes[100];
     while (displayMenu)
     {
         int choice;
@@ -19,10 +21,22 @@ int main() {
         switch (choice) {
             case 1:
                 printf("Adding a password...\n");
+                printf("Enter the website name: \n");
+                scanf(" %s", websiteName);
+                printf("Enter the website URL: \n");
+                scanf(" %s", URL);
+                printf("Enter the username: \n");
+                scanf(" %s", userName);
+                printf("Enter the password: \n");
+                scanf(" %s", password);
+                //printf("Enter notes: \n");
+                //scanf(" %s", notes);
+                addRecord(fileName, websiteName, URL, userName, password, notes);
                 break;
 
             case 2:
                 printf("Viewing all passwords...\n");
+                readWholeFile(fileName);
                 break;
 
             case 3:
