578d02933ef9100d70117b5f1c555b8126e93088
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2024-04-09 12:12:38 +0200

    file cleanup

--- main.c
+++ main.c
@@ -43,6 +43,4 @@ int main() {
 
     return 0;
     }
-    
-    
 }
--- pw_manager.h
+++ pw_manager.h
@@ -1,14 +1,17 @@
 #ifndef PW_MANAGER_H
 #define PW_MANAGER_H
 
+//pw_menuHandler.c
 void displayMainMenu();
 
+//db_handler.c
 void readWholeFile(char fileName[100]);
 void addRecord(char fileName[100], char websiteName[100], char URL[100], char userName[100], char password[100], char notes[100]);
 void deleteRecord(char fileName[100], int LineNo);
 void getComponent(char fileName[100], int row, int col);
 void changeComponent(char fileName[100], char changedComponent[100], int row, int column);
 
+//pw_encryption.c
 void encryptPassword(char* password, int key);
 void decryptPassword(char* password, int key);
 
