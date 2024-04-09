11290ce8c0d7a92b597e045f022c078829f1e65e
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2024-04-09 12:39:16 +0200

    cast to char and key variable fix

--- pw_encryption.c
+++ pw_encryption.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define KEY 5
+#define key 5
 
 
 void encryptPassword(char* password) {
     int i = 0;
     while (password[i] != '\0') {
         if (password[i] >= 'a' && password[i] <= 'z') {
-            password[i] = (password[i] - 'a' + key) % 26 + 'a';
+            password[i] = (char)((password[i] - 'a' + key) % 26 + 'a');
         } else if (password[i] >= 'A' && password[i] <= 'Z') {
-            password[i] = (password[i] - 'A' + key) % 26 + 'A';
+            password[i] = (char)((password[i] - 'A' + key) % 26 + 'A');
         }
         i++;
     }
@@ -21,9 +21,9 @@ void decryptPassword(char* password) {
     int i = 0;
     while (password[i] != '\0') {
         if (password[i] >= 'a' && password[i] <= 'z') {
-            password[i] = (password[i] - 'a' - key + 26) % 26 + 'a';
+            password[i] = (char)((password[i] - 'a' - key + 26) % 26 + 'a');
         } else if (password[i] >= 'A' && password[i] <= 'Z') {
-            password[i] = (password[i] - 'A' - key + 26) % 26 + 'A';
+            password[i] = (char)((password[i] - 'A' - key + 26) % 26 + 'A');
         }
         i++;
     }
