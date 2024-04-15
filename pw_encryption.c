ea30766c41a8b11c3c439afdd11ad3cc41160066
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2024-04-15 19:07:42 +0200

    ASCII Encryption

--- pw_encryption.c
+++ pw_encryption.c
@@ -9,13 +9,7 @@ char* pw_encrypt(const char* message) {
     char* encrypted = (char*)malloc((length + 1) * sizeof(char));
 
     for (size_t i = 0; i < length; i++) {
-        if (message[i] >= 'a' && message[i] <= 'z') {
-            encrypted[i] = (char)('a' + (message[i] - 'a' + key) % 26);
-        } else if (message[i] >= 'A' && message[i] <= 'Z') {
-            encrypted[i] = (char)('A' + (message[i] - 'A' + key) % 26);
-        } else {
-            encrypted[i] = message[i];
-        }
+        encrypted[i] = (char)((message[i] + key) % 256);
     }
 
     encrypted[length] = '\0';
@@ -27,13 +21,7 @@ char* pw_decrypt(const char* encrypted) {
     char* decrypted = (char*)malloc((length + 1) * sizeof(char));
 
     for (size_t i = 0; i < length; i++) {
-        if (encrypted[i] >= 'a' && encrypted[i] <= 'z') {
-            decrypted[i] = (char)('a' + (encrypted[i] - 'a' - key + 26) % 26);
-        } else if (encrypted[i] >= 'A' && encrypted[i] <= 'Z') {
-            decrypted[i] = (char)('A' + (encrypted[i] - 'A' - key + 26) % 26);
-        } else {
-            decrypted[i] = encrypted[i];
-        }
+        decrypted[i] = (char)((encrypted[i] - key + 256) % 256);
     }
 
     decrypted[length] = '\0';
